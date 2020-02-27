9280e010822553f5a7218c4038f47dcd01ad1f04
Martin Møller Skarbiniks Pedersen <traxplayer@gmail.com>
2020-02-27 14:38:07 +0100

    Create robustLog.R

---
+++ try-catch/robustLog.R
@@ -0,0 +1,18 @@
+inputs <- list(1, 2, 4, -5, 'oops', 0, 10)
+
+robustLog <- function(num) {
+    tryCatch( log(num), 
+		warning = function(warn) {
+		    print(paste("negative argument", num))
+		    NaN
+		},
+		error = function(err) {
+		    print(paste("non-numeric argument", num))
+		    NaN
+		}
+	    )
+}
+
+for (input in inputs) {
+    print(paste("log of", input, "=", robustLog(input)))
+}
